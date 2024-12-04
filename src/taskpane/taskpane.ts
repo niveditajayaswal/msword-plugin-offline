2bccfe0bfda0d3456f9624883408da295573ac77
kumar-shivang <shivangkumar1857@gmail.com>
2024-12-04 18:36:14 +0530

    Add functions for document manipulation and text insertion in taskpane

--- src/taskpane/taskpane.ts
+++ src/taskpane/taskpane.ts
@@ -1,12 +1,84 @@
 /* global Word console */
+import { LoremIpsum } from "lorem-ipsum";
 
 export async function insertText(text: string) {
-  // Write text to the document.
   try {
     await Word.run(async (context) => {
       let body = context.document.body;
       body.insertParagraph(text, Word.InsertLocation.end);
       await context.sync();
+    });
+  } catch (error) {
+    console.log("Error: " + error);
+  }
+}
+
+export async function getSelectedData(): Promise<string> {
+  let text = "";
+  try {
+    await Word.run(async (context) => {
+      const range = context.document.getSelection();
+      range.load("text");
+      await context.sync();
+      text = range.text;
+    });
+  } catch (error) {
+    console.log("Error: " + error);
+  }
+  return text;
+}
+
+export async function getDocumentText() {
+  try {
+    await Word.run(async (context) => {
+      const body = context.document.body;
+      body.load("text");
+      await context.sync();
+      return body.text;
+    });
+  } catch (error) {
+    console.log("Error: " + error);
+  }
+}
+
+export async function writeToDocument(text: string) {
+  try {
+    await Word.run(async (context) => {
+      const body = context.document.body;
+      body.insertText(text, Word.InsertLocation.end);
+      await context.sync();
+    });
+  } catch (error) {
+    console.log("Error: " + error);
+  }
+}
+
+export async function clearDocument() {
+  try {
+    await Word.run(async (context) => {
+      const body = context.document.body;
+      body.clear();
+      await context.sync();
+    });
+  } catch (error) {
+    console.log("Error: " + error);
+  }
+}
+
+export async function addLoremIpsum() {
+  const lorem = new LoremIpsum();
+  const text = lorem.generateSentences(5);
+  insertText(text);
+}
+
+export async function addParagraph() {
+  try {
+    await Word.run(async (context) => {
+      const body = context.document.body;
+      let paragraph = body.paragraphs.getLast();
+      paragraph = paragraph.insertParagraph("New paragraph", Word.InsertLocation.after);
+      paragraph.font.bold = true;
+      paragraph.font.color = "blue";
     });
   } catch (error) {
     console.log("Error: " + error);
