ed011043d1c9ba330d0cc2b55fda04e333c3c4a5
kumar-shivang <shivangkumar1857@gmail.com>
2024-12-04 18:36:22 +0530

    Integrate socket.io for real-time functionality and replace TextInsertion with SelectedText component

--- src/taskpane/components/App.tsx
+++ src/taskpane/components/App.tsx
@@ -1,10 +1,17 @@
 import * as React from "react";
 import Header from "./Header";
-import HeroList, { HeroListItem } from "./HeroList";
 import TextInsertion from "./TextInsertion";
+import SelectedText from "./SelectedText";
 import { makeStyles } from "@fluentui/react-components";
-import { Ribbon24Regular, LockOpen24Regular, DesignIdeas24Regular } from "@fluentui/react-icons";
 import { insertText } from "../taskpane";
+import { getSelectedData } from "../taskpane";
+import io from "socket.io-client";
+
+const socket = io("http://localhost:5000");
+
+socket.on("connect", () => {
+  console.log("Connected to server");
+});
 
 interface AppProps {
   title: string;
@@ -18,28 +25,11 @@ const useStyles = makeStyles({
 
 const App: React.FC<AppProps> = (props: AppProps) => {
   const styles = useStyles();
-  // The list items are static and won't change at runtime,
-  // so this should be an ordinary const, not a part of state.
-  const listItems: HeroListItem[] = [
-    {
-      icon: <Ribbon24Regular />,
-      primaryText: "Achieve more with Office integration",
-    },
-    {
-      icon: <LockOpen24Regular />,
-      primaryText: "Unlock features and functionality",
-    },
-    {
-      icon: <DesignIdeas24Regular />,
-      primaryText: "Create and visualize like a pro",
-    },
-  ];
-
   return (
     <div className={styles.root}>
       <Header logo="assets/logo-filled.png" title={props.title} message="Welcome" />
-      <HeroList message="Discover what this add-in can do for you today!" items={listItems} />
-      <TextInsertion insertText={insertText} />
+      {/* <TextInsertion insertText={insertText} /> */}
+      <SelectedText getSelectedText={getSelectedData} />
     </div>
   );
 };
